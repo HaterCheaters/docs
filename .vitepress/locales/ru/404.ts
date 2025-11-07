fe3f343facfe86f4e43dbf8a04b45a5df1f78c11
HaterCheaters <AlexK995@gmail.com>
2025-11-07 22:17:55 +0300

    ru

---
+++ .vitepress/locales/ru/404.ts
@@ -0,0 +1,9 @@
+import type { DefaultTheme } from 'vitepress'
+
+const notFound: DefaultTheme.NotFoundOptions = {
+  title: 'Страница не найдена',
+  quote: 'Может быть, Венти взял её в путешествие.',
+  linkText: 'Перейти на главную страницу',
+}
+
+export default notFound
