8b0f2ecc6bb043ee13d2ff0b4515d2a1ce8bd2b3
HaterCheaters <AlexK995@gmail.com>
2025-11-07 22:42:51 +0300

    ru

---
+++ .vitepress/locales/ru/constants.ts
@@ -0,0 +1,13 @@
+import type { CustomConstant } from '../types'
+
+const constants: CustomConstant = {
+  META_URL: 'https://yuanshen.site/docs/ru',
+  META_TITLE: 'Таверна Kongying',
+  META_DESCRIPTION: 'Полная карта-путеводитель по ресурсам Genshin Impact от таверны Kongying.',
+  META_KEYWORDS: 'Интерактивная карта Genshin, Карта Kongying, Genshin Impact Kongying, ресурсы Genshin Impact, yuanshen, Таверна Kongying',
+  META_IMAGE: 'https://yuanshen.site/docs/imgs/common/cover.jpg',
+  LOCAL_CODE: 'ru-RU',
+  LOCAL_BASE: 'ru',
+}
+
+export default constants
