f48d13de1b405af5de7609b67c8d9ccbc6d0f7be
HaterCheaters <AlexK995@gmail.com>
2025-11-08 18:42:54 +0300

    ru

---
+++ .vitepress/locales/ru/docs-feedback.ts
@@ -0,0 +1,28 @@
+import type { CustomConfig } from '../types'
+
+const docReaction: CustomConfig['docReaction'] = {
+  feedbackMsg: 'Эта инормация была полезна?',
+  good: 'Да',
+  bad: 'Нет',
+  feedbackFailMsg: 'Не удалось отправить отзыв. Повторите попытку или свяжитесь с администратором（QQ：1961266616）!',
+  feedbackSuccessMsg: 'Отзыв отправлен, спасибо за неравнодушие!',
+  badFeedbackSuccessMsg: 'Пожалуйста дайте нам узнать подробности',
+  form: {
+    chooseIssues: 'С какими трудностями вы столкнулись?',
+    translationIssue: 'Некорректный перевод',
+    typosIssue: 'Опечатки / ошибки пунктуации',
+    contentImgLinkIssue: 'Ошибка формулировок, неверные изображение или нерабочие ссылки',
+    feedbackDetail: 'Обратная связь / предложение по улучшению',
+    feedbackTip: 'Пожалукйста подробно опишите проблему или предложение по улучшению.',
+    otherIssue: 'Другие проблемы (пожалуйста подробно опишите в отзыве)',
+    contactWay: 'Контактная информация（опционально）',
+    issueOptions: [
+      { label: 'Ошибка отображения', value: 'CATA-DISPLAY' },
+      { label: 'Опечатки / ошибки пунктуации', value: 'CATA-TYPOS' },
+      { label: 'Неверный контент / неработающие ссылки', value: 'CATA-DOCS' },
+      { label: 'Другие проблемы', value: 'CATA-OTHER' },
+    ],
+  },
+}
+
+export default docReaction
