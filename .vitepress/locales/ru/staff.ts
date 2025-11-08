71aa9e441bdc69c76985e114a685eadbf1f2005f
HaterCheaters <AlexK995@gmail.com>
2025-11-08 13:45:19 +0300

    ru

---
+++ .vitepress/locales/ru/staff.ts
@@ -0,0 +1,23 @@
+import type { CustomConfig } from '../types'
+
+const staff: CustomConfig['staff'] = {
+  title: 'Участники',
+  desc: 'Участники таверны Kongying принявшие участие в создании карты для Genshin Impact',
+  communityStaff: {
+    title: 'Благодарность за помощь в развитии проекта',
+  },
+  clientStaff: {
+    title: 'Front-end разработка (клиент)',
+  },
+  webStaff: {
+    title: 'Back-end разработка (сервер)',
+  },
+  translateStaff: {
+    title: 'Локализатоця',
+  },
+  pinStaff: {
+    title: 'Картография',
+  },
+}
+
+export default staff
