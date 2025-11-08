d7f47e9789dd3fa8eedf759e18077b19ca0bfadc
HaterCheaters <AlexK995@gmail.com>
2025-11-08 13:26:15 +0300

    ru

---
+++ .vitepress/locales/ru/aside-links.ts
@@ -0,0 +1,13 @@
+import type { CustomConfig } from '../types'
+
+const asideLinks: CustomConfig['asideLinks'] = {
+  title: 'Ссылки',
+  starOnGitHub: 'Star on GitHub',
+  contactUsText: 'Заходи на наш Discord сервер',
+  contactUsLink: 'https://discord.gg/SWz6RTWNkm',
+  sponsor: 'Стать спонсором',
+  editLink: 'Редактировать',
+  translateThisPage: 'Помочь улучшить переводом',
+}
+
+export default asideLinks
